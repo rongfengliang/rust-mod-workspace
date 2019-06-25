a1a5e988fc3383b2c2cb4ae6b9ad8bc0fd8e8452
rongfengliang <1141591465@qq.com>
2019-06-25 09:13:28 +0800

    add default demo

--- lib/usersmain/src/app/add.rs
+++ lib/usersmain/src/app/add.rs
@@ -1,6 +1,19 @@
 #[derive(Debug)]
 pub struct Add{
+  pub first: i32,
+  pub second: i32,
+  pub third: i32,
+}
 
+// impl for default 
+impl Default for Add {
+   fn default() -> Add {
+      Add {
+         first:0,
+         second:0,
+         third:0,
+      }
+   }
 }
 impl Add {
    pub fn add(first: u32, second: u32) -> u32 {
--- lib/usersmain/src/main.rs
+++ lib/usersmain/src/main.rs
@@ -13,7 +13,9 @@ use libmods::appdemos::MyAppdemo;
 fn main() {
     let appmodinfo =MyAppdemo{appname:"dlaongdemo".into(),appversion: "v1.0".into()};
     let moddemo =UserLogin{name:"dlaongdemo".into()};
-    let addop = Add{};
+    // default demo
+    let addop: Add = Add {third:444, ..Default::default()};
+    print!("{:?}",addop);
     let result = Add::add(12,44);
     let result2 = addop.add2(33,444);
     print!("{:?},\r\n{},\r\n{}",appmodinfo,result,result2);
